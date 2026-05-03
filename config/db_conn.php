b230242afac5f63c790a338a2ef68b60425b5e8b
Nino <107749@vistacollege.nl>
2026-05-03 14:19:33 +0200

    .

--- config/db_conn.php
+++ config/db_conn.php
@@ -8,6 +8,16 @@ try {
     $pdo->exec('PRAGMA foreign_keys = ON');
     $pdo->exec('PRAGMA journal_mode = WAL');
 
+    $pdo->exec('CREATE TABLE IF NOT EXISTS apod_entries (
+    date TEXT PRIMARY KEY,
+    title TEXT,
+    url TEXT,
+    hdurl TEXT,
+    media_type TEXT,
+    explanation TEXT,
+    copyright TEXT
+)');
+
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
